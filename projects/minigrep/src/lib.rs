d2c8d6ae22aa7b5f94fc8bef79094bf7aca7ad8c
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-11-15 15:04:53 +0000

    redid minigrep example

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -1,74 +1,37 @@
-use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
-#[cfg(test)]
-mod tests;
-
 pub struct Config {
     query: String,
-    filename: String,
-    pub case_sensitive: bool
+    filename: String
 }
 
 impl Config {
-    pub fn new(args: &[String], case_sensitive: bool) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() != 3 {
+            return Err("Please provide exactly 2 arguments to this application.")
         }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        return Ok(Config {query, filename, case_sensitive});
+    let query : String = args.get(1)
+    .expect("Must supply exactly 2 arguments for minigrep.")
+    .clone();
+
+    let filename : String = args.get(2) 
+    .expect("Must supply exactly 2 arguments for minigrep.")
+    .clone();
+        Ok(Config {query, filename})
     }
 }
 
-
-
-pub fn run(config: Config) -> Result<(), Box<Error>>{
-
-    let mut f = File::open(config.filename)?;
-
-
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename).expect("File not found");
 
     let mut contents = String::new();
+
     f.read_to_string(&mut contents)?;
 
-    let results = if config.case_sensitive {
-        search(&config.query, &contents) 
-    } else {
-        search_case_insensitive(&config.query, &contents)
-    };
 
-    for line in results {
-        println!("{}", line);
-    }
+    println!("With text:\n{}", contents);
 
-    return Ok(());
-
-}
-
-
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    return results;
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-    
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    return results;
+    Ok({})
 }
--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -1,28 +1,18 @@
 extern crate minigrep;
-
 use std::env;
-use std::fs::File;
-use std::process;
-use std::io::prelude::*;
-use std::error::Error;
-
+use std::process::exit;
 use minigrep::Config;
-use minigrep::run;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();;
-
-    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-    let config: Config = Config::new(&args, case_sensitive).unwrap_or_else(|err| {
+    let args: Vec<String> = env::args().collect();
+    let config : Config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
-                 process::exit(1);
+        exit(1);
     });
-            
-   if let Err(e) = run(config) {
-       println!("Application error: {}", e);
-       process::exit(1);
-   }
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application Error: {}", e);
+        exit(-1);
+    }
+
 }
-
-
-
