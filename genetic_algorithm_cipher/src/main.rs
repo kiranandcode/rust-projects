2cd267c2d802f85f7ddba1ccae2ed481156bbe76
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-12-09 14:35:27 +0000

    Added encrypted test file + loading

--- genetic_algorithm_cipher/src/main.rs
+++ genetic_algorithm_cipher/src/main.rs
@@ -17,6 +17,8 @@ use genetic_algorithm_cipher::GA::{
     GeneticAlgorithm
 };
 
+use std::fs::File;
+use std::io::Read;
 use genetic_algorithm_cipher::GA::strategy::HillClimbing::{ HillClimbingStrategy};
 use genetic_algorithm_cipher::GA::strategy::SteepestAscentHillClimbing::{ SteepestAscentHillClimbingStrategy };
 use genetic_algorithm_cipher::GA::strategy::SteepestAscentReplacementHillClimbing::{ SteepestAscentReplacementHillClimbingStrategy};
@@ -97,23 +99,33 @@ fn main() {
 //    println!("corpus score: {}", frequency.score_text(&corpus));
 //    println!("text score: {}", frequency.score_text(&text));
 //    println!("cipherA score: {}", frequency.score_text(&text_A));
-    let corpus = get_corpus().unwrap(); 
+//    let mut text = "ljsboibtxsjuufobtjnqmftvctujuvujpodjqifs".to_string();
+//    let mut copy = text.clone();
+    let corpus = get_corpus().unwrap();
     let frequency = NgramFrequency::generate_from(&corpus, 3);
-    let mut text = "ljsboibtxsjuufobtjnqmftvctujuvujpodjqifs".to_string();
-    let mut copy = text.clone();
-    let encryption = SubstitutionCipher::new();
-    let alternate = encryption.apply(&corpus);
-    
-    println!("Original: {}", alternate);
+
+    let mut encrypted_text = if let Ok(mut file) = File::open("encrypted.txt") {
+        let mut result: String = String::new();
+
+        file.read_to_string(&mut result);
+
+        result
+    } else {
+        println!("Couldn't find ./encrypted.txt");
+
+        "".to_string()
+    };
+
+//    println!("Original: {}", alternate);
     let generator = CipherGenerator {};
     let evaluator = CipherEvaluator {
         frequency_table: frequency,
-        encrypted_text: alternate.clone()
+        encrypted_text: encrypted_text.clone()
     };
 
     let mut genetic_algorithm = GeneticAlgorithm::new(generator, evaluator);
     let mut hill_climbing = SteepestAscentReplacementHillClimbingStrategy::new(20, 10);
     let best = genetic_algorithm.execute_strategy(&mut hill_climbing);
-    println!("Decrypted: {}", best.cipher.apply(&alternate));
+    println!("Decrypted: {}", best.cipher.apply(&encrypted_text));
 
 }
