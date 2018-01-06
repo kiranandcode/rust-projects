eb7eadbd1a0e1b33100d896bb85048a532300b97
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-06 21:39:17 +0000

    added tests and iterative drop

--- list_learning/src/first.rs
+++ list_learning/src/first.rs
@@ -123,6 +123,13 @@ impl List {
                    // to avoid this, we want to get the whole node
                    // once this is on the stack, rust can allow us to retrieve internal values 
                    // as though they were variables
+                   // this is a move out of box operation
+                   // equivalent to
+                   // struct node *boxed_node= some_pointer;
+                   // struct node node;
+                   // node.elem = boxed_node->elem;
+                   // node.next = boxed_node->next;
+                   // free(boxed_node);
                    let node = *boxed_node;
                    // move the node.next variable out, 
                    self.head = node.next;
@@ -131,5 +138,52 @@ impl List {
                    Some(node.elem) 
             }
         }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        // first retrieve the nullable pointer from the list struct
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+
+        // iterative list deletion
+        // while((boxed_node != null)) {
+        //     struct node *current_node = boxed_node;
+        //  // the following two lines are achieved using the mem::replace
+        //     boxed_node = boxed_node->next;
+        //     current_node->next = null;
+        //  // the free occurs automatically by the rust compiler
+        //    free(boxed_node);
+        // }
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
