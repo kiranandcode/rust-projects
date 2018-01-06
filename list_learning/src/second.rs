a0b8dd787ea45e27adbd4af38e02d926da3cb197
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-06 21:53:36 +0000

    added generic list with optional type

---
+++ list_learning/src/second.rs
@@ -0,0 +1,74 @@
+use std::mem;
+
+pub struct List<T> {
+    head: Link<T>
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>
+}
+
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List {
+            head: None
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next:  self.head.take()
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+                let node = *node;
+                self.head = node.next;
+                node.elem
+        })
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut current_node = self.head.take();
+
+        while let Some(mut boxed_node) = current_node {
+            current_node = boxed_node.next.take();
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::List;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
