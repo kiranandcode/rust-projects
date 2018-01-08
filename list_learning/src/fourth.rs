029048c70361429e72cdaffeb0460ef6a14246e4
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-08 08:31:05 +0000

    added back variant functions

--- list_learning/src/fourth.rs
+++ list_learning/src/fourth.rs
@@ -94,6 +94,36 @@ impl<T> List<T> {
         })
     }
 
+    pub fn push_back(&mut self, elem: T) {
+        let new_tail = Node::new(elem);
+        match self.tail.take() {
+            Some(old_tail) => {
+                old_tail.borrow_mut().next = Some(new_tail.clone());
+                new_tail.borrow_mut().prev = Some(old_tail);
+                self.tail = Some(new_tail);
+            }
+            None => {
+                self.head = Some(new_tail.clone());
+                self.tail = Some(new_tail);
+            }
+        }
+    }
+
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.take().map(|old_tail| {
+            match old_tail.borrow_mut().prev.take() {
+                Some(new_tail) => {
+                    new_tail.borrow_mut().next.take();
+                    self.tail = Some(new_tail);
+                }
+                None => {
+                    self.head.take();
+                }
+            }
+            Rc::try_unwrap(old_tail).ok().unwrap().into_inner().elem
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
