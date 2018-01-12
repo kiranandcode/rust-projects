c1ba4c96bc749ca4573d588fb422acbb711f85ae
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-12 19:43:14 +0000

    added passing fast code with unsafe blocks

--- list_learning/src/fifth.rs
+++ list_learning/src/fifth.rs
@@ -1,8 +1,9 @@
 use std::mem;
+use std::ptr;
 
-pub struct List<'a, T : 'a> {
+pub struct List<T> {
     head: Link<T>,
-    tail: Option<&'a mut Node<T>>
+    tail: *mut Node<T>
 }
 
 type Link<T> = Option<Box<Node<T>>>;
@@ -12,40 +13,44 @@ struct Node<T> {
     next: Link<T>
 }
 
-impl <'a, T> List<'a, T> {
+impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None, tail: None }
+        List {head: None, tail: ptr::null_mut()}
+    }
+    pub fn push(&mut self, elem: T) {
+        let mut new_tail = Box::new(Node {
+            elem: elem,
+            next: None
+        });
+
+        let raw_tail: *mut _ = &mut *new_tail;
+
+        // if tail != null, tail->next = new_tail;
+        if !self.tail.is_null() {
+            // must be derefed as tail is a raw pointer
+            unsafe {
+            (*self.tail).next = Some(new_tail);
+            }
+        } else {
+            // else self.head = new_tail
+            self.head = Some(new_tail);
+        }
+        self.tail = raw_tail;
     }
 
-    pub fn push(&'a mut self, elem: T) {
-        let new_tail = Box::new(Node { elem: elem, next: None});
-        
-        let new_tail = match self.tail.take() {
-            Some(old_tail) => {
-                old_tail.next = Some(new_tail);
-                // return a reference to the new tail by dereferencing it and taking a ref
-                old_tail.next.as_mut().map(|node| &mut **node)
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|head| {
+            let head = *head;
+            self.head = head.next;
+            if self.head.is_none() {
+                self.tail = ptr::null_mut();
             }
-            None => {
-                self.head = Some(new_tail);
-                self.head.as_mut().map(|node| &mut **node)
-            }
-        };
-        self.tail = new_tail;
-   }
 
-   pub fn pop(&'a mut self) -> Option<T> {
-       self.head.take().map(|head| {
-           let head = *head;
-           self.head = head.next;
-
-           if self.head.is_none() {
-               self.tail = None;
-           }
-           head.elem
-       })
-   }
+            head.elem
+        })
+    }
 }
+
 
 #[cfg(test)]
 mod test {
@@ -59,7 +64,6 @@ mod test {
         list.push(1);
         list.push(2);
         list.push(3);
-        list.push(4);
 
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), Some(2));
