a0e9b1650f278c7c091d85cf9ebc218b7b2fb0c8
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-01-15 23:28:48 +0000

    Rust learning

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,4 +1,7 @@
 use std::collections::HashMap;
+mod extra;
+use extra::BinaryTree;
+
 type Table = HashMap<String, Vec<String>>;
 fn show(table: &Table) {
     for(artist, works) in table {
@@ -17,6 +20,49 @@ fn smallest(v: &[i32]) -> &i32 /* lifetime elision */ {
     }
     s
 }
+
+fn example() {
+    let mut x = 10;
+    let r1 = &x;
+    let r2 = &x;
+    // can not modify x as it is borrowed
+    // x += 10;
+    // can not borrow x as mutable as it is borrowed
+    // let m = &mut x;
+}
+
+fn other_example() {
+    let mut v = (107, 109);
+    let r = &v;
+    let r0 = &r.0; // reborrow okay
+    let rr0 = &v.0; // parent path is read only access - can make more refs but not mutable or mutate
+}
+
+fn expression_first() {
+    let mut i = 0;
+    return loop {
+        println!("i <- {}", i);
+        i = i + 1;
+            if i > 3 {
+                break;
+            }
+    };
+}
+
+fn divergent_function() -> ! {
+    loop {
+        println!("Looping forever");
+    }
+}
+
+fn pattern_matching() {
+    let string = match "a string" {
+        "a string" => "str",
+        other      => other
+    };
+}
+
+
 fn main() {
     // rust has several integer types
     let mut v:u8 = 10;
@@ -36,6 +82,10 @@ fn main() {
     println!("Hello, world!");
 
     println!("Smallest {} ", smallest(&arr));
+
+    let mut tree = BinaryTree::new();
+    tree.add("Mercury");
+    tree.add("Venus");
 
 }
 
