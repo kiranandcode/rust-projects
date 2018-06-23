997f8b11b8523f1166eef16a8da39e4b8535a343
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2018-06-23 23:58:20 +0100

    began writeup of problem at a game jam, had to cheap out again.

--- algorithm_problem/src/main.rs
+++ algorithm_problem/src/main.rs
@@ -1,3 +1,17 @@
+// UVA 10363 - Tic Tac Toe Projects
+// 
+// Tic Tac Toe is played on a 3x3 grid.
+// obvious rulse are used.
+//
+// denote the Tic Tac Toe grid with nine dots
+// whenever either player plays fill with x or 0
+//
+//
+// read a grid and determine whether it could possibly be valid
+
+// Define struct to contains object
+
+
 fn main() {
     println!("Hello, world!");
 }
